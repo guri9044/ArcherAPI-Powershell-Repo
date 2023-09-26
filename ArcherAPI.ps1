09c6df2f3c7dc9ef2c05ccca75a02b34e2aeafb0
Gurdas Singh <guri.9044@gmail.com>
2023-09-26 09:32:35 +0530

    2

--- ArcherAPI.ps1
+++ ArcherAPI.ps1
@@ -1,12 +1,11 @@
-﻿class AResponse
-{
+﻿#Import-Module .\ArcherContent.ps1
+class AResponse {
     [string]$Value
     [bool]$IsSuccessful
     [object]$Exception
     [object]$Response
 
-    AResponse($Value,$IsSuccessful,$Exception,$Response)
-    {
+    AResponse($Value, $IsSuccessful, $Exception, $Response) {
         $this.Value = $Value
         $this.IsSuccessful = $IsSuccessful
         $this.Exception = $Exception
@@ -18,103 +17,66 @@ class ArcherAPI {
     [object] $response
     [string] $sessionToken
 
-    ArcherAPI([string] $baseURL)
-    {
+    ArcherAPI([string] $baseURL) {
         $this.baseURL = $baseURL
     }
 
-    [AResponse] Login([string]$userName,[string]$password,[string]$instanceName,$userDomain )
-    {
-        try
-        {
-            $requestURL = $this.baseURL+'/platformapi/core/security/login'
-            #$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-            $headers = @{
-                "Content-Type" ="application/json"
-                }
+    [AResponse] Login([string]$userName, [string]$password, [string]$instanceName, $userDomain ) {
+        try {
+            $requestURL = $this.baseURL + '/platformapi/core/security/login'
+            $headers = @{"Content-Type" = "application/json" }
 
             $body = @{
                 "InstanceName" = $instanceName
-                "Username" = $userName
-                "UserDomain" = $userDomain
-                "Password" = $password
+                "Username"     = $userName
+                "UserDomain"   = $userDomain
+                "Password"     = $password
             } | ConvertTo-Json
-
-            #$body = $body | ConvertTo-Json
             $this.response = Invoke-RestMethod $requestURL -Method 'POST' -Headers $headers -Body $body
-            if($this.response.IsSuccessful -eq $True)
-            {
-                $this.sessionToken = $this.response.RequestedObject.SessionToken
-                $okResult = [AResponse]::new($this.sessionToken,$this.response.IsSuccessful,'',$this.response)
-                return $okResult
-            }
-            else
-            {
+            if ($this.response.IsSuccessful -eq $False) {
                 throw $this.response.ValidationMessages.MessageKey
             }
+            $this.sessionToken = $this.response.RequestedObject.SessionToken
+            $okResult = [AResponse]::new($this.sessionToken, $this.response.IsSuccessful, '', $this.response)
+            return $okResult
         }
-        catch
-        {
-            $failResult = [AResponse]::new($_.Exception.Message,$this.response.IsSuccessful,$_.Exception,$this.response)
+        catch {
+            $failResult = [AResponse]::new($_.Exception.Message, $this.response.IsSuccessful, $_.Exception, $this.response)
             return $failResult
         }
     }
 
-    [AResponse] Logout([string]$sessionToken)
-    {
-        try
-        {
-            $requestURL = $this.baseURL+'/platformapi/core/security/logout'
-            $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-            $headers.Add("Content-Type", "application/json")
+    [AResponse] Login2([string]$userName, [string]$password, [string]$instanceName, $userDomain ) {
+        try {
+            $okResult = [AResponse]::new($userName, $password, '', $instanceName)
+            return $okResult
+        }
+        catch {
+            $okResult = [AResponse]::new($userName, $password, '', $instanceName)
+            return $okResult
+        }
+    }
 
+    [AResponse] Logout() {
+        try {
+            $requestURL = $this.baseURL + '/platformapi/core/security/logout'
+            $headers = @{
+                "Content-Type"  = "application/json"
+                "Authorization" = "Archer session-id=`"" + $this.sessionToken + "`""
+            }
             $body = @{
-                "Value" = $sessionToken
-            }
-
-            $body = $body | ConvertTo-Json
+                "Value" = $this.sessionToken
+            } | ConvertTo-Json
             $this.response = Invoke-RestMethod $requestURL -Method 'POST' -Headers $headers -Body $body
-            if($this.response.IsSuccessful -eq $True)
-            {
-                $okResult = [AResponse]::new('KO',$this.response.IsSuccessful,'',$this.response)
-                return $okResult
-            }
-            else
-            {
+            if ($this.response.IsSuccessful -eq $False) {
                 throw $this.response.ValidationMessages.MessageKey
             }
+            $okResult = [AResponse]::new('KO', $this.response.IsSuccessful, '', $this.response)
+            return $okResult
         }
-        catch
-        {
-            $failResult = [AResponse]::new($_.Exception.Message,$this.response.IsSuccessful,$_.Exception,$this.response)
+        catch {
+            $failResult = [AResponse]::new($_.Exception.Message, $this.response.IsSuccessful, $_.Exception, $this.response)
             return $failResult
         }
     }
-}
-
-
-Clear-Host
-
-$archerAPI = [ArcherAPI]::new('http://192.168.44.10/Archer')
-$loginResponse = $archerAPI.Login( 'webapi', 'Archer@123', 'oda','')
-
-if($loginResponse.IsSuccessful -eq $True)
-{
-    $sessionToken = $archerAPI.sessionToken
-    Write-Output 'Authentication Successful' $sessionToken ''
-}
-elseif ($loginResponse.IsSuccessful -eq $False)
-{
-    Write-Output 'Authentication Unsuccessful' $loginResponse.Exception
-}
-
-$logoutResponse = $archerAPI.Logout($sessionToken);
-
-if($logoutResponse.IsSuccessful -eq $True)
-{
-    Write-Output 'KO'
-}
-elseif ($logoutResponse.IsSuccessful -eq $False)
-{
-    Write-Output 'Unable to KO' $logoutResponse.Value $logoutResponse.Exception $logoutResponse.IsSuccessful $logoutResponse.Response
 }
