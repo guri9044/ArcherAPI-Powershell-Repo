f00fa7eb3759d95efef9c54c74d547a4e174c265
Gurdas Singh <guri.9044@gmail.com>
2023-09-28 20:08:17 +0530

    1

--- ArcherSearch.ps1
+++ ArcherSearch.ps1
@@ -30,12 +30,15 @@ class ArcherSearch {
                 "Accept"                  = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
                 "Content-Type"            = "text/xml"
             }
-            $body = "<?xml version=`"1.0`" encoding=`"utf-8`"?><soap12:Envelope xmlns:xsi=`"http://www.w3.org/2001/XMLSchema-instance`" xmlns:xsd=`"http://www.w3.org/2001/XMLSchema`" xmlns:soap12=`"http://www.w3.org/2003/05/soap-envelope`"><soap12:Body><SearchRecordsByReport xmlns=`"http://archer-tech.com/webservices/`"><sessionToken>"+$this.sessionToken+"</sessionToken><reportIdOrGuid>"+$reportIDorGUID+"</reportIdOrGuid><pageNumber>"+$pageNumber+"</pageNumber></SearchRecordsByReport></soap12:Body></soap12:Envelope>"
+            $body = "<?xml version=`"1.0`" encoding=`"utf-8`"?><soap12:Envelope xmlns:xsi=`"http://www.w3.org/2001/XMLSchema-instance`" xmlns:xsd=`"http://www.w3.org/2001/XMLSchema`" xmlns:soap12=`"http://www.w3.org/2003/05/soap-envelope`"><soap12:Body><SearchRecordsByReport xmlns=`"http://archer-tech.com/webservices/`"><sessionToken>" + $this.sessionToken + "</sessionToken><reportIdOrGuid>" + $reportIDorGUID + "</reportIdOrGuid><pageNumber>" + $pageNumber + "</pageNumber></SearchRecordsByReport></soap12:Body></soap12:Envelope>"
             $this.response = Invoke-RestMethod $requestURL -Method 'POST' -Headers $headers -Body $body
             $xmlResponse = [XML]$this.response
             $responseResult = $xmlResponse.Envelope.Body.SearchRecordsByReportResponse.SearchRecordsByReportResult
-            $decodedResponse = [System.Net.WebUtility]::HtmlDecode($responseResult)
-            $okResult = [AResponse]::new($decodedResponse, $True, '', $this.response)
+            PrivateExtractRecordsfromXML($responseResult)
+
+
+
+            $okResult = [AResponse]::new($responseResult, $True, '', $this.response)
             return $okResult
         }
         catch {
@@ -45,3 +48,10 @@ class ArcherSearch {
     }
 }
 
+function PrivateExtractRecordsfromXML([string] $inputXML) {
+    $xmlObject = [XML]$inputXML
+    $blankJsonObject = @{}
+    $jsonData = $blankJsonObject | ConvertTo-Json
+    $countOfRecords = $xmlObject.Records.Attributes["count"].Value
+    Clear-Host
+}
--- Test.ps1
+++ Test.ps1
@@ -5,10 +5,9 @@ Import-Module .\ArcherSearch.ps1
 Clear-Host
 
 $archerAPI = [ArcherAPI]::new('http://192.168.44.10/Archer')
-$loginResponse = $archerAPI.Login( 'webapi', 'Archer@123', 'oda','')
-if($loginResponse.IsSuccessful -eq $False)
-{
-    throw 'Authentication Unsuccessful'+ $loginResponse.Exception
+$loginResponse = $archerAPI.Login( 'webapi', 'Archer@123', 'oda', '')
+if ($loginResponse.IsSuccessful -eq $False) {
+    throw 'Authentication Unsuccessful' + $loginResponse.Exception
 }
 $sessionToken = $archerAPI.sessionToken
 
@@ -28,7 +27,9 @@ $reqJSON = $rJSON.Create(34,0)
 Write-Host $reqJSON
 $recordID  = $archerContent.Record_Create($reqJSON).Value #>
 
-$aO = [ArcherSearch]::new('http://192.168.44.10/Archer',$sessionToken)
-$aO.SearchRecordsByReport(13794,1)
+$aO = [ArcherSearch]::new('http://192.168.44.10/Archer', $sessionToken)
+$ko = $aO.SearchRecordsByReport(13794, 1).Value
+Clear-Host
+Write-Host $ko
+Write-Host 'END"'
 
-Write-Host 'End'
